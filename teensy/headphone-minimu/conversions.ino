329ce2e8be2886cfb1821243ab4ceec4f5750485
random-entity <73927400+random-entity@users.noreply.github.com>
2022-01-17 11:55:12 +0900

    misc cleanup

--- teensy/headphone-minimu/conversions.ino
+++ teensy/headphone-minimu/conversions.ino
@@ -1,11 +1,3 @@
-void IntTo4Bytes(int n, byte *arr)
-{
-    *arr = (n >> 24) & 0xFF;
-    *(arr + 1) = (n >> 16) & 0xFF;
-    *(arr + 2) = (n >> 8) & 0xFF;
-    *(arr + 3) = n & 0xFF;
-}
-
 void ShortTo2Bytes(short n, byte *arr)
 {
     *arr = (n >> 8) & 0xFF;
